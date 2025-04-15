07a8cb42fad054d780a0b59231f62f8f3b21fe4b
q-1-p <github.pubilc@outlook.com>
2025-04-15 19:42:11 +0900

    feat: Modify functional types

--- lib/option.ts
+++ lib/option.ts
@@ -1,4 +1,4 @@
-export const some = Symbol();
-export const none = Symbol();
+export const some = Symbol("some");
+export const none = Symbol("none");
 
 export type Option<T> = { type: typeof some; value: T } | { type: typeof none };
--- lib/result.ts
+++ lib/result.ts
@@ -1,5 +1,5 @@
-export const ok = Symbol();
-export const err = Symbol();
+export const ok = Symbol("ok");
+export const err = Symbol("err");
 
 export type Result<T, E> =
   | { type: typeof ok; value: T }
