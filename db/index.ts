24c671f6ee2d1cc1715fe8fda7b7da46931756ae
q-1-p <github.pubilc@outlook.com>
2025-05-10 09:54:18 +0900

    feat: Delete deploy setting

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,10 @@
-import { Pool, neon } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { drizzle as drizzleSocket } from "drizzle-orm/neon-serverless";
 
 import * as schema from "./schema";
 
-export const db = drizzle(neon(process.env.DATABASE_URL as string), { schema });
+export const db = drizzle(process.env.DATABASE_URL as string, { schema });
 
 export const dbSocket = drizzleSocket({
   client: new Pool({ connectionString: process.env.DATABASE_URL }),
