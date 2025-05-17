fa7e63f9b99224e2fb163559e5bf71028a7444f8
q-1-p <github.pubilc@outlook.com>
2025-05-17 21:24:57 +0900

    feat: Write privacy policy

---
+++ app/(public)/privacy-policy/page.tsx
@@ -0,0 +1,25 @@
+import fs from "node:fs";
+import path from "node:path";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import SitePublicHeader from "@/components/domain/site/site-public-header";
+
+export default function PrivacyPolicyPage() {
+  // legal.mdファイルの内容を読み込む
+  const filePath = path.join(process.cwd(), "public/doc/privacy-policy.md");
+  const content = fs.readFileSync(filePath, "utf8");
+
+  return (
+    <>
+      <SitePublicHeader />
+      <div className="flex min-h-screen flex-col items-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8 dark:bg-gray-900">
+        <div className="mb-10 w-full max-w-4xl overflow-hidden rounded-lg bg-white p-6 shadow-lg sm:p-8 md:p-10 dark:bg-gray-800">
+          <article className="prose prose-sm dark:prose-invert mx-auto prose-p:my-1 prose-headings:mb-2 max-w-3xl text-left prose-headings:font-bold prose-a:text-blue-600 prose-headings:text-gray-800 leading-relaxed hover:prose-a:text-blue-800 dark:prose-a:text-blue-400 dark:prose-headings:text-gray-100">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+}
