be2854a115e37cb07e2611a2270d3c0acd66d5f1
q-1-p <github.pubilc@outlook.com>
2025-06-22 17:28:18 +0900

    feat: Add “day” to update cycle unit

--- test/small/components/subscription-billing-information-card.test.tsx
+++ test/small/components/subscription-billing-information-card.test.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, test } from "bun:test";
 import { render, screen } from "@testing-library/react";
+import { getDaysInMonth, getDaysInYear } from "date-fns";
 
 import type {
   CancellationMethodId,
@@ -10,6 +11,31 @@ import { currencyId, timeUnit } from "@/domain/type";
 import SubscriptionBillingInformationCardPresentation from "components/domain/subscription/subscription-billing-information-card.presentation";
 
 describe("SubscriptionBillingInformationCardPresentation", () => {
+  test("日次サブスクコスト計算", () => {
+    const mock: SubscriptionDetail = {
+      id: "1",
+      name: "Netflix",
+      active: true,
+      fee: 1500,
+      amount: 100,
+      currencyId: currencyId.usd,
+      updateCycle: { number: 2, unit: timeUnit.day } as UpdateCycle,
+      nextUpdate: new Date("2023-12-01"),
+      linkCancellationMethodId: "1" as CancellationMethodId,
+    };
+
+    render(
+      <SubscriptionBillingInformationCardPresentation subscription={mock} />,
+    );
+
+    // 各種表示内容の検証
+    const today = new Date();
+    const dailyCost = mock.fee / mock.updateCycle.number;
+    const monthlyCost = dailyCost * getDaysInMonth(today);
+    const yearlyCost = dailyCost * getDaysInYear(today);
+    expect(screen.getByText(`¥${monthlyCost.toLocaleString()}`)).toBeTruthy();
+    expect(screen.getByText(`¥${yearlyCost.toLocaleString()}`)).toBeTruthy();
+  });
   test("月次サブスクコスト計算", () => {
     const mock: SubscriptionDetail = {
       id: "1",
