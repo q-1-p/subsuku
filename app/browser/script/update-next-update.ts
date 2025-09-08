f82b2d175f075ff4f29e6090e476f2cb0afa5232
q-1-p <github.pubilc@outlook.com>
2025-09-08 09:27:23 +0900

    feat: Modify update next-update

--- app/browser/script/update-next-update.ts
+++ app/browser/script/update-next-update.ts
@@ -1,8 +1,39 @@
-import { SubscriptionRepository } from "@/infrastructure/subscription-repository";
+import { addMonths, addYears, format } from "date-fns";
+import { eq, lt } from "drizzle-orm";
+
+import { db } from "@/db";
+import { subscriptionsTable } from "@/db/schema";
+import { timeUnit } from "@/domain/type";
+
+const updateNextUpdate = async () => {
+  const today = format(new Date(), "yyyy-MM-dd");
+  const updateSubscriptions = await db
+    .select({
+      id: subscriptionsTable.id,
+      nextUpdate: subscriptionsTable.nextUpdate,
+      updateCycleNumber: subscriptionsTable.updateCycleNumber,
+      updateCycleUnit: subscriptionsTable.updateCycleUnit,
+    })
+    .from(subscriptionsTable)
+    .where(lt(subscriptionsTable.nextUpdate, today));
+
+  for (const subscription of updateSubscriptions) {
+    const nextUpdate =
+      subscription.updateCycleUnit === timeUnit.month
+        ? addMonths(subscription.nextUpdate, subscription.updateCycleNumber)
+        : addYears(subscription.nextUpdate, subscription.updateCycleNumber);
+
+    await db
+      .update(subscriptionsTable)
+      .set({
+        nextUpdate: format(nextUpdate, "yyyy-MM-dd"),
+      })
+      .where(eq(subscriptionsTable.id, subscription.id));
+  }
+};
 
 if (require.main === module) {
-  new SubscriptionRepository()
-    .updateNextUpdate()
+  updateNextUpdate()
     .then(() => {
       console.log("次回更新日更新プロセスが正常に完了しました");
       process.exit(0);
